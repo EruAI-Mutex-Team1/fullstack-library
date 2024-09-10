6bc0f22bbf7c5da4e75d196bbc6154e5721a4546
turkerkiv <kivturker@gmail.com>
2024-09-10 22:50:52 +0300

    Revert "required entities added"
    This reverts commit e64407eb4db3786847d0b0fe0e49b7bd28310810.

--- libraryApp.backend/Entity/Author.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace libraryApp.backend.Entity
-{
-    public class Author
-    {
-        public int id { get; set; }
-        public int bookId { get; set; }
-        public int memberId { get; set; }
-    }
-}
--- libraryApp.backend/Entity/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace libraryApp.backend.Entity
-{
-    public class Role
-    {
-        public int id { get; set; }
-        public string name { get; set; } = string.Empty;
-    }
-}
