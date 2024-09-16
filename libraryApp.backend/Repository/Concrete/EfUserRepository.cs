11417c6bc94eeab6477d49bf79f01fd515d13a8a
feyzabyts <beyaztasfyz@gmail.com>
2024-09-16 23:28:27 +0300

    I changed some line in methods

--- libraryApp.backend/Repository/Concrete/EfUserRepository.cs
+++ libraryApp.backend/Repository/Concrete/EfUserRepository.cs
@@ -1,6 +1,53 @@
-﻿namespace libraryApp.backend.Repository.Concrete
+﻿using libraryApp.backend.Entity;
+using libraryApp.backend.Repository.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace libraryApp.backend.Repository.Concrete
 {
-    public class EfUserRepository
+    public class EfUserRepository : IUserRepository
     {
+        public IQueryable<User> Users => _context.Users;
+        private readonly LibraryDbContext _context;
+        
+
+        public async Task CreateUserAsync(User user)
+        {
+
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async  Task DeleteUserAsync(int id)
+        {
+            var efUser = _context.Users.FindAsync(id);
+            if (efUser != null)
+            {
+                _context.Remove(efUser);
+                await _context.SaveChangesAsync();
+
+            }
+            else
+                throw new ArgumentException("User doesn't found.");
+            
+
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> GetUseridAsync(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.id == id);
+        }
     }
 }
