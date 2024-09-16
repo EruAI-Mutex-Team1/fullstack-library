978ae912d867001cb28eab9ef791d87e965cecc8
feyzabyts <beyaztasfyz@gmail.com>
2024-09-16 20:42:03 +0300

    I wrote some methods

---
+++ libraryApp.backend/Repository/Concrete/EfRegisterRequestRepository.cs
@@ -0,0 +1,62 @@
+﻿using libraryApp.backend.Entity;
+using libraryApp.backend.Repository.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace libraryApp.backend.Repository.Concrete
+{
+    public class EfRegisterRequestRepository : IRegisterRequestRepository
+    {
+
+
+        public IQueryable<RegisterRequest> GetAllRegisterRequests => _context.RegisterRequests;
+        private readonly LibraryDbContext _context;
+
+        public EfRegisterRequestRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Role> GetAdminByIdAsync(int id)
+        {
+            return await _context.Roles.FindAsync(id);
+        }
+
+        public async Task<RegisterRequest> GetByIdAsync(int id)
+        {
+            return await _context.RegisterRequests.FirstOrDefaultAsync(x => x.id == id);
+        }
+
+        public async Task UpdateAsync(RegisterRequest record)
+        {
+            if (record == null)
+                throw new ArgumentNullException(nameof(record));
+
+            _context.RegisterRequests.Update(record);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task CreateRegisterRequestAsync(RegisterRequest record)
+        {
+
+            if (record == null)
+                throw new ArgumentNullException(nameof(record));
+
+            _context.RegisterRequests.Add(record);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(RegisterRequest record)
+        {
+            var efRegisterRequest = await _context.RegisterRequests.FindAsync(record);
+            if (efRegisterRequest != null)
+            {
+                _context.RegisterRequests.Remove(efRegisterRequest);
+                await _context.SaveChangesAsync();
+            }
+            else
+                throw new ArgumentException("Record doesn't found");
+            
+        }
+    }
+}
