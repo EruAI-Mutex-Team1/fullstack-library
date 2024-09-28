1b3c53077160072c89da91a4bc443ddd4357afd0
Zehraulker07 <151157467+Zehraulker07@users.noreply.github.com>
2024-09-28 13:19:09 +0300

    add rest of the issues

--- libraryApp.frontend/src/App.jsx
+++ libraryApp.frontend/src/App.jsx
@@ -1,16 +1,29 @@
 
-import WritePage2 from "./WritePage2"
+//import InboxPage2 from "./InboxPage2"
+import Register2 from "./Register2"
+//import Login from "./Login"
+//import WritePage2 from "./WritePage2"
 
 function App() {
 
   return (
     
     <>
-      
+      {/* <div>
+        <Login/>
+      </div> */}
 
       <div>
-        <WritePage2/>
+        <Register2/>
       </div>
+
+      {/* <div>
+        <InboxPage2/>
+      </div> */}
+
+      {/* <div>
+        <WritePage2/>
+      </div> */}
     </>
     
   )
